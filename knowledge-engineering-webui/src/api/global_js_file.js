5c0cb41174db7773418864183193674dd849f20b
wangjt <445220457@qq.com>
2020-03-15 17:16:52 +0800

    knowledge

--- knowledge-engineering-webui/src/api/global_js_file.js
+++
@@ -1,2 +0,0 @@
-//引入iconfont.js
-import iconF from '../assets/font_plug_in/iconfont'
--- knowledge-engineering-webui/src/assets/data/flow-data/data_N.js
+++
@@ -1,8 +0,0 @@
-var data_N = {
-    nodeList: [],
-    lineList: []
-};
-
-export function getDataN() {
-    return data_N
-}
